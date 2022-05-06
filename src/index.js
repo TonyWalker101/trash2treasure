6d65484e88d68ff11f2fd4cc3594a1d6e5cf573f
isissi <isissi_525@hotmail.com>
2022-05-06 15:20:48 -0600

    add routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import App from "./App";
+import NotFound from "./pages/404";
+import SearchResult from "./pages/SearchResult";
+import React from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById("root");
+render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/404" element={ <NotFound /> }/>
+        <Route path="/SearchResult" element={ <SearchResult /> }/>
+
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/SearchResult.jsx
@@ -0,0 +1,9 @@
+const SearchResult = () => {
+  return(
+    <div>
+      <h1>Search Result</h1>
+    </div>
+  )
+}
+
+export default SearchResult;
