0f1c726eb7df35770ddf1ac5a4b902ff2ee398ab
isissi <isissi_525@hotmail.com>
2022-05-06 19:50:46 -0600

    modify routes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,12 @@
-import { render } from "react-dom";
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter as Router} from "react-router-dom";
+import "./index.css";
 import App from "./App";
-import NotFound from "./pages/404";
-import SearchResult from "./pages/SearchResult";
-import AddNew from "./pages/AddNew";
-import React from "react";
 
-const rootElement = document.getElementById("root");
-render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="*" element={ <NotFound /> }/>
-        <Route path="/" element={<App />} />
-        <Route path="/search-result" element={ <SearchResult /> }/>
-        <Route path="/add-new" element={<AddNew />} />
-
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-  rootElement
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
