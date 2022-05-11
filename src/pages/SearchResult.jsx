27c270a8bbcc418955dcc69e1d9194ea241713c2
Tony <awalker780@gmail.com>
2022-05-10 22:41:01 -0400

    Add DisplayMap and TreasureList components

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,10 +1,16 @@
 import SearchHeader from "../components/layout/SearchHeader";
+import DisplayMap from "../components/layout/DisplayMap";
+import TreasureList from "../components/layout/TreasureList";
 
 const SearchResult = () => {
+
+
   return(
     <div>
       <SearchHeader />
-      <h1>Search Result</h1>
+      {/* <h1>Search Result</h1> */}
+      <DisplayMap />
+      <TreasureList />
     </div>
   )
 }
