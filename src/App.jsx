96d74511f7685a484a156b9d6b942823427b9daf
isissi <isissi_525@hotmail.com>
2022-05-11 18:13:57 -0600

    remove unused import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Switch, Link, Redirect } from "react-router-dom"
 import NotFound from "./pages/404"
