0e3d79e2c59fe6ef3eb21d25c3f46db57a74d131
Tony <awalker780@gmail.com>
2022-05-12 22:54:30 -0400

    Create AddMapDonations component for /search-results

---
+++ src/components/AddNew/AddMapDonations.jsx
@@ -0,0 +1,56 @@
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React from "react";
+import mapStyles from "../../mapStyles";
+// import axios from 'axios';
+
+const containerStyle = {
+  width: '50vw',
+  height: '91vh'
+};
+
+const center = {
+  lat: 43.6532,
+  lng: -79.3832
+};
+
+const AddNewMap = () => {
+
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const options = {
+    styles: mapStyles, 
+    disableDefaultUI: true,
+    zoomControl: true,
+  }
+
+  return isLoaded ?(
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={5}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={options}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>    
+  ) : <></>
+}
+
+export default AddNewMap;
