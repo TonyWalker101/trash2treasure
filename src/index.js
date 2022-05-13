4c775febaca5c231390ed7abfc30393e3a8187fd
isissi <isissi_525@hotmail.com>
2022-05-13 13:45:11 -0600

    initialize timeAgo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { BrowserRouter as Router} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+// import timeago
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
+
+TimeAgo.addDefaultLocale(en)
+
 ReactDOM.render(
   <Router>
     <App />
