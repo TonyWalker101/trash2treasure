477dcc392778a1f6f029362b819c69669820cb90
Tony <awalker780@gmail.com>
2022-05-12 22:30:18 -0400

    Add AddNewMap and Treasures components to /search-results page

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,10 +1,14 @@
+import AddNewMap from "../components/AddNew/AddNewMap";
 import SearchHeader from "../components/layout/SearchHeader";
+import TreasureList from "../components/Treasures/TreasureList";
 
 const SearchResult = () => {
   return(
     <div>
       <SearchHeader />
       <h1>Search Result</h1>
+      <AddNewMap/>
+      <TreasureList/>
     </div>
   )
 }
