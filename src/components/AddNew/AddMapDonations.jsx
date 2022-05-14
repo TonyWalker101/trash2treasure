a29ff35a0ef6a846061a6cc9c4bd8f469997aea7
Tony <awalker780@gmail.com>
2022-05-14 14:35:51 -0400

    Add useState to component

--- src/components/AddNew/AddMapDonations.jsx
+++ src/components/AddNew/AddMapDonations.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 import mapStyles from "../../mapStyles";
 import axios from 'axios';
+import Geocode from "react-geocode"
+
+// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey("AIzaSyAy1SXOH3JNtfc-VbNZ3-OX0iAGctZ0hls");
+
+// set response language. Defaults to english.
+Geocode.setLanguage("en");
 
 const containerStyle = {
   width: '50vw',
@@ -17,6 +24,18 @@ const center = {
 const AddNewMap = () => {
 
   const [state, setState] = useState([])
+
+  const location = "180 Wellington Avenue Toronto";
+
+  Geocode.fromAddress(location).then(
+    (response) => {
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log("these are the coordinates:", lat, lng);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
 
   useEffect(() => {
     Promise.all([
