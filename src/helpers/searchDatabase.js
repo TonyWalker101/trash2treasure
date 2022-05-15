d915c4dfde671074fb4fda542eedb47155fc470e
Tony <awalker780@gmail.com>
2022-05-15 14:51:54 -0400

    Add functionality to search logic

--- src/helpers/searchDatabase.js
+++ src/helpers/searchDatabase.js
@@ -1,4 +1,5 @@
 import Geocode from "react-geocode"
+import JsSearch from "js-search";
 
 // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
 Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
@@ -6,14 +7,30 @@ Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 // set response language. Defaults to english.
 Geocode.setLanguage("en");
 
-const location = "180 Wellington Avenue Toronto";
+// const location = "180 Wellington Avenue Toronto";
 
-Geocode.fromAddress(location).then(
-  (response) => {
-    const { lat, lng } = response.results[0].geometry.location;
-    console.log("these are the coordinates:", lat, lng);
-  },
-  (error) => {
-    console.error(error);
-  }
-);
+const getGeocode = location => {
+
+  Geocode.fromAddress(location).then(
+    (response) => {
+      const { lat, lng } = response.results[0].geometry.location;
+      console.log("these are the coordinates:", lat, lng);
+    },
+    (error) => {
+      console.error("An error occured:", error);
+    }
+  );
+}
+
+const searchButtonClicked = (form) => {
+
+  // const searchLocation = getGeocode(form);
+
+  getGeocode(form.location);
+  // console.log("this is the event form:", form)
+  console.log(`Looking for ${form.item}?`)
+  console.log(`...Searching in ${form.location}:`);
+
+};
+
+export default searchButtonClicked;
