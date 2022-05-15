c1f6733f85aaf992c7761fe11d7b81f6d89ac31e
Tony <awalker780@gmail.com>
2022-05-15 16:29:55 -0400

    Implement search and return state to parent component

--- src/helpers/searchDatabase.js
+++ src/helpers/searchDatabase.js
@@ -1,8 +1,9 @@
 import Geocode from "react-geocode"
-import JsSearch from "js-search";
+import * as JsSearch from "js-search";
+import listData from "../__mocks__/list";
 
 // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
-Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API);
 
 // set response language. Defaults to english.
 Geocode.setLanguage("en");
@@ -22,14 +23,52 @@ const getGeocode = location => {
   );
 }
 
+// const search = new JsSearch.Search('name');
+// search.addIndex('description');
+// const searchData = listData.map(item => item.id = item);
+// search.addDocument(searchData);
+
+const searchDB = (data, item) => {
+  if (!item) {
+    return;
+  }
+
+  const searchableItem = item.toLowerCase();
+
+  const matchingResults = data.filter(treasure => {
+    // if (treasure.name.toLowerCase().includes(searchableItem) || treasure.description.toLowerCase().includes(searchableItem)) {
+    //   return treasure;
+    // } else {
+    //   ;
+    // }
+
+    return treasure.name.toLowerCase().includes(searchableItem) || treasure.description.toLowerCase().includes(searchableItem);
+  });
+  
+  return matchingResults;
+  
+};
+
+
 const searchButtonClicked = (form) => {
 
   // const searchLocation = getGeocode(form);
+  if (form.location) {
+    getGeocode(form.location);
+    console.log(`...Searching in ${form.location}:`);
+  }
 
-  getGeocode(form.location);
+  if (form.item) {
+    console.log(`Looking for ${form.item}?`)
+    console.log(searchDB(listData, form.item));
+    const results = searchDB(listData, form.item);
+    return results;
+  }
   // console.log("this is the event form:", form)
-  console.log(`Looking for ${form.item}?`)
-  console.log(`...Searching in ${form.location}:`);
+
+
+  // console.log("searchData Array:", searchData)
+  // console.log("items that match your search:", search.search("2"));
 
 };
 
