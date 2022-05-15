16d3adcbbbfc3fccf16fe2d7098773e7e470afb0
Tony <awalker780@gmail.com>
2022-05-15 14:52:23 -0400

    Add event handling for searched items and location

--- src/components/layout/SearchHeader.jsx
+++ src/components/layout/SearchHeader.jsx
@@ -2,8 +2,24 @@ import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../theme';
+import searchButtonClicked from '../../helpers/searchDatabase';
+import { useState } from "react";
 
 const HomeHeader = () => {
+
+  const [search, setSearch] = useState({
+    location: null,
+    item: null
+  });
+
+  const handleLocationInputChanged = e => {
+    setSearch(prev => ({...prev, location: e}));
+  }
+
+  const handleItemInputChanged = e => {
+    setSearch(prev => ({...prev, item: e}));
+  }
+
   return(
     <header>
       <nav id="search-nav">
@@ -17,12 +33,12 @@ const HomeHeader = () => {
         <form class="header-search-bar">
           <ThemeProvider theme={theme}>
           
-          <TextField id="filled-basic" label="Search by location" variant="filled" size="small"   InputProps={{ disableUnderline: true}}/>
+          <TextField id="filled-basic" label="Search by location" variant="filled" size="small"   InputProps={{ disableUnderline: true}} onChange={(event) => handleLocationInputChanged(event.target.value)}/>
           <div class="vl"></div>
 
-          <TextField id="filled-basic" label="Search by item" variant="filled" size="small" InputProps={{ disableUnderline: true }}/>
+          <TextField id="filled-basic" label="Search by item" variant="filled" size="small" InputProps={{ disableUnderline: true }} onChange={(event) => handleItemInputChanged(event.target.value)}/>
 
-            <Button variant="contained" disableElevation color="primary" className="search-button" sx={{ borderRadius: 8 }}><i class="fa-solid fa-magnifying-glass fa-xl"></i></Button>
+            <Button variant="contained" disableElevation color="primary" className="search-button" sx={{ borderRadius: 8 }} onClick={() => searchButtonClicked(search)}><i class="fa-solid fa-magnifying-glass fa-xl"></i></Button>
           </ThemeProvider>
         </form>
     </header>
