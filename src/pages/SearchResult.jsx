cda4e8165f286d3b2997ae7da6d5244a0dbce051
isissi <isissi_525@hotmail.com>
2022-05-14 20:51:14 -0600

    add button onclick to modal

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -10,15 +10,13 @@ const SearchResult = () => {
   const [markers, setMarkers] = useState([]);
   const [selected, setSelected] = useState(null);
   const [modal, setModal] = useState(null);
-  const handleClose = () => setModal(null);
-
 
   return(
     <div>
       <SearchHeader />
       <SearchResultMap listData={listData} setMarkers={setMarkers} setSelected={setSelected} markers={markers} selected={selected}/>
       <ResultList listData={listData} selected={selected} modal={modal} setModal={setModal}/>
-      <DetailsModal modal={modal} setModal={setModal} handleClose={handleClose}/>
+      <DetailsModal modal={modal} setModal={setModal} listData={listData}/>
     </div>
   )
 }
