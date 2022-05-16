51b581a8f7d5c96656b927efe5185719c21df9e0
isissi <isissi_525@hotmail.com>
2022-05-15 21:29:31 -0600

    add comment display section & styles

--- src/components/SearchResult/CommentSection.jsx
+++ src/components/SearchResult/CommentSection.jsx
@@ -1,8 +1,34 @@
-const CommentSection = () => {
+import ReactTimeAgo from 'react-time-ago'
+import { Typography,Divider } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../../theme';
 
+
+const CommentSection = (props) => {
+  const length = props.comments.length;
+  const commentTag  = () => {
+    if (length === 0 || length === 1) {
+      return length + " comment";
+    } else {
+      return length + " comments";
+    }
+  }
   return (
     <div>
-      <p>comments</p>
+      <ThemeProvider theme={theme}>
+      <Divider><Typography variant='modalText'><b>{commentTag()}</b> </Typography></Divider>
+      {props.comments && 
+        props.comments.map((data, index) => {
+          return(
+            <section className="comment-section" key={index}>
+              <Typography variant='modalText'>{data.comment}</Typography>
+              <Typography variant='modalText'>Posted by {data.id}</Typography>
+              <Typography variant='modalText'>Posted <ReactTimeAgo date={Date.parse(data.created_at)} locale="en-US"/></Typography>
+            </section>
+          )}
+        )
+      }
+      </ThemeProvider>
     </div>
   )
 }
--- src/components/SearchResult/DetailsModal.jsx
+++ src/components/SearchResult/DetailsModal.jsx
@@ -21,8 +21,9 @@ const DetailsModal = (props) => {
 
   const userName = getUserNameByItemId(props.users, props.listData, props.modal)
 
-  // console.log(selectedData);
+  const comments = getCommentsByItemId(props.comments, props.modal);
 
+  // console.log(comments);
   return(
     <Modal
       open={setOpen()}
@@ -33,53 +34,56 @@ const DetailsModal = (props) => {
     >
       <>
       <ThemeProvider theme={theme}>
+      
         <div className="modal-container">
+          <div className="item-info-container">
+            <div className="button-container">
+              <button onClick={()=>props.setModal(null)} className="close-button"><i class="fa-solid fa-xmark fa-2xl"></i></button>
+            </div>
 
-          <div className="button-container">
-            <button onClick={()=>props.setModal(null)} className="close-button"><i class="fa-solid fa-xmark fa-2xl"></i></button>
+            <div className="details-container">
+              <div id="details-image-crop">
+                {selectedData && <img src={selectedData.image} id="modal-img"/>}
+              </div>
+
+              <div className="details-text-containter">
+                  <Typography variant="modalTitle" sx={{mb: 5}}>{selectedData && selectedData.name}</Typography>
+
+                  <Typography variant="modalText" sx={{mb: 2}}><b>Location: </b>{selectedData && selectedData.location}</Typography>
+
+                  <Typography variant="modalText" sx={{mb: 2}}><b>Condition: </b>{selectedData && selectedData.condition}</Typography>
+
+                  <Typography variant="modalText" sx={{mb: 2}}><b>Description: </b>{selectedData && selectedData.description}</Typography>
+
+                  <Typography variant="modalText" sx={{mb: 2}}><b>Posted by: </b>{userName}</Typography>
+
+                  <Typography variant="modalText" sx={{mb: 10}}>Posted {selectedData && <ReactTimeAgo date={Date.parse(selectedData.created_at)} locale="en-US"/>}</Typography>
+                  
+                  
+                  {(claim === null)?
+                      <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="claim-button" color="primary" onClick={() => setClaim(props.modal)} sx={{mt: 1}}>Claim</Button>
+                
+                  :(
+                      <div className="confirm-container">
+                        <Typography variant="modalText" sx={{mb: 10}}><b>Are you sure you are at the location & want to claim the item?</b></Typography> 
+                        <div className="confirm-button-group">
+                          <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="confirm-button" color="primary" onClick={() => setClaim(null)}>Cancel</Button>
+        
+                          <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="confirm-button" color="secondary" onClick={() => setClaim(null)}>Confirm</Button>
+                        </div>
+                    </div>
+                  )}
+                  
+                  
+                  
+              </div>
+            </div>
           </div>
 
-          <div className="details-container">
-            <div id="details-image-crop">
-              {selectedData && <img src={selectedData.image} id="modal-img"/>}
-            </div>
-
-            <div className="details-text-containter">
-                <Typography variant="modalTitle" sx={{mb: 5}}>{selectedData && selectedData.name}</Typography>
-
-                <Typography variant="modalText" sx={{mb: 2}}><b>Location: </b>{selectedData && selectedData.location}</Typography>
-
-                <Typography variant="modalText" sx={{mb: 2}}><b>Condition: </b>{selectedData && selectedData.condition}</Typography>
-
-                <Typography variant="modalText" sx={{mb: 2}}><b>Description: </b>{selectedData && selectedData.description}</Typography>
-
-                <Typography variant="modalText" sx={{mb: 2}}><b>Posted by: </b>{userName}</Typography>
-
-                <Typography variant="modalText" sx={{mb: 10}}>Posted {selectedData && <ReactTimeAgo date={Date.parse(selectedData.created_at)} locale="en-US"/>}</Typography>
-                
-                
-                {(claim === null)?
-                    <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="claim-button" color="primary" onClick={() => setClaim(props.modal)} sx={{mt: 1}}>Claim</Button>
-               
-                :(
-                    <div className="confirm-container">
-                      <Typography variant="modalText" sx={{mb: 10}}><b>Are you sure you are at the location & want to claim the item?</b></Typography> 
-                      <div className="confirm-button-group">
-                        <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="confirm-button" color="primary" onClick={() => setClaim(null)}>Cancel</Button>
-      
-                        <Button variant="contained" fontWeight="fontWeightRegular" disableElevation className="confirm-button" color="secondary" onClick={() => setClaim(null)}>Confirm</Button>
-                      </div>
-                  </div>
-                )}
-                
-                
-                
-            </div>
+          <div className="comment-container">
+          <CommentSection comments={comments}/>
           </div>
         </div>
-            <div>
-            <CommentSection />
-            </div>
       </ThemeProvider>
       </>
     </Modal>
