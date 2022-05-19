66a09c327d1984fe37e4b686ffef5653c68d2c26
Tony <awalker780@gmail.com>
2022-05-19 00:23:55 -0400

    Adds state for marker and passes props to components

--- src/pages/AddNew.jsx
+++ src/pages/AddNew.jsx
@@ -2,21 +2,40 @@ import SearchHeader from "../components/layout/SearchHeader";
 import AddNewMap from "../components/AddNew/AddNewMap";
 import AddNewForm from "../components/AddNew/AddNewForm";
 import "../stylesheet/AddNew.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import convertAddressToLink from "../helpers/getCoordinateByAddress"
 import CommentSection from "../components/SearchResult/CommentSection";
+import getAddress from "../helpers/getAddress"
 
 const AddNew = () => {
   const [location, setLocation] = useState({
-    address: '', 
+    address: "", 
     lat: "",
     log: "" 
   });
 
+  const [marker, setMarker] = useState({
+    lat: null,
+    lng: null}
+    )
+    
+    const handleClick = event => {
+
+      setMarker({lat: event.latLng.lat(), lng: event.latLng.lng()});
+
+      const geocode = {lat: event.latLng.lat(), lng: event.latLng.lng()}
+
+      //gets address from latitude/longitude from Google Maps click
+      getAddress(geocode)
+      .then((data) => {
+        return setLocation({address: data})
+      })
+    }
+  
   return (
     <div>
       <SearchHeader />
-      <AddNewMap />   
+      <AddNewMap marker={marker} onClick={(e) => handleClick(e)}/>   
       <AddNewForm convertAddressToLink={convertAddressToLink} location={location} setLocation={setLocation}/> 
     </div>
   )
